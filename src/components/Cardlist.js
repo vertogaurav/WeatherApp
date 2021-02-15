399443fd67d7c742264c24957aaa87520891f93f
vertogaurav <vertogaurav@gmail.com>
2021-02-16 00:19:00 +0530

    Evaluation commit

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -1,15 +1,69 @@
 import React from 'react'
-
+import { Card } from "react-bootstrap";
+import TimeIcon from './wi-time-3.svg';
 
 function CityDetails(props) {
     console.log(props);
-    const detail= props.location.state.details;
+    const detail = props.location.state.details;
     return (
         <div className="result">
-            <div>{detail.name}</div>
-            <div>{detail.main.temp}°Cel</div>
-            <div>{detail.weather[0].description}</div>
+            <div className="sideNav">    
+                <h2>{detail.name}</h2>
+                <div><img src={TimeIcon}  className="ImgDeatail"/></div>
+                <div>Temp - {detail.main.temp}°Cel</div>
+                <div>Sky - {detail.weather[0].description}</div>
+            </div>
+           <div className="Card">
+            <div> 
+                <Card className="content">
+                <Card.Body>
+                <Card.Title>Temprature</Card.Title>
+                <Card.Text>
+                <div>
+                Min-{detail.main.temp_min}°Cel
+                </div>
+                <div>
+                Max-{detail.main.temp_max}°Cel
+                </div>
+                </Card.Text>
+                
+            </Card.Body>
+            </Card>
+            </div>
+            <div> 
+                <Card className="content">
+                <Card.Body>
+                <Card.Title>Wind Status</Card.Title>
+                <Card.Text>
+                <div>
+                Min-{detail.wind.speed}km/hr
+                </div>
+                <div>
+                Max-{detail.main.temp_max}°Cel
+                </div>
+                </Card.Text>
+                
+            </Card.Body>
+            </Card>
+            
+            </div>
+            <div> 
+                <Card className="content">
+                <Card.Body>
+                <Card.Title>Humidity</Card.Title>
+                <Card.Text>
+                <div>
+                {detail.main.humidity}%
+                </div>
+                
+                </Card.Text>
+                
+            </Card.Body>
+            </Card>
+            
+            </div>
            
+            </div>
 
         </div>
     )
